c6914fde61fe61ff0fdf10190d0c22175d36f37f
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-07-24 13:04:06 +0330

    feat: category table

---
+++ database/migrations/2022_02_28_093914_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Enumerations\Category\Status;
+use App\Enumerations\Category\Visiblity;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('parent_id')->nullable()->constrained('categories');
+            $table->string('slug')->unique();
+            $table->tinyInteger('visible')->default(Visiblity::VISIBLE->value);
+            $table->tinyInteger('status')->default(Status::ACTIVE->value);
+            $table->json('path')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('image')->nullable();
+            $table->tinyInteger('level')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedTinyInteger('position')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
