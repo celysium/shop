0ea6c979c097b75e0ed234a7c41d84ad3105f3a0
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-07-24 18:50:50 +0330

    fix: banner model

---
+++ app/Models/Banner.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use App\Enumerations\Banner\Status;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property int $slider_id
+ * @property string $path
+ * @property string $title
+ * @property string $url
+ * @property int $position
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class Banner extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'slider_id',
+        'path',
+        'title',
+        'url',
+        'position',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => Status::class,
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function slider(): BelongsTo
+    {
+        return $this->belongsTo(Slider::class);
+    }
+}
