914ddd4deff8ac5deffc9c47c2575125996e1e64
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-07-24 13:46:22 +0330

    fix: store add address

--- database/migrations/2023_01_24_075655_create_stores_table.php
+++ database/migrations/2023_01_24_075655_create_stores_table.php
@@ -16,6 +16,15 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('image')->nullable();
+            $table->foreignId('province_id')->constrained('locations');
+            $table->foreignId('city_id')->constrained('locations');
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
+            $table->tinyText('detail');
+            $table->string('postcode')->nullable();
+            $table->string('plate_number')->nullable();
+            $table->string('floor')->nullable();
+            $table->string('unit')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
