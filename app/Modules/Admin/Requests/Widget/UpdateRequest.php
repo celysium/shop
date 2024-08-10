4fab652b3b4ba166e12f6751e6fa21ed2d1f82f1
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-08-10 09:07:54 +0330

    fix: add admin widget update request

---
+++ app/Modules/Admin/Requests/Widget/UpdateRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\Admin\Requests\Widget;
+
+use App\Modules\Core\Enumerations\Widget\Status;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class UpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name'   => ['required', 'string'],
+            'slug'   => ['sometimes', 'string', 'unique:widgets,slug'],
+            'icon'   => ['sometimes', 'file', 'max:200'],
+            'image'  => ['sometimes', 'file', 'max:500'],
+            'status' => ['required', new Enum(Status::class)],
+        ];
+    }
+}
