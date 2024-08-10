a2707160ecd58a1250997387e43d0dc858445eb1
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-08-10 09:07:42 +0330

    fix: add admin widget store request

---
+++ app/Modules/Admin/Requests/Widget/StoreRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\Admin\Requests\Widget;
+
+use App\Modules\Core\Enumerations\Widget\Status;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class StoreRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name'   => ['required', 'string'],
+            'slug'   => ['sometimes', 'string', 'unique:widgets,slug'],
+            'icon'   => ['nullable', 'file', 'max:200'],
+            'image'  => ['nullable', 'file', 'max:500'],
+            'status' => ['required', new Enum(Status::class)],
+        ];
+    }
+}
