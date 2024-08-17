861b2d8a6c3564835244f241134bf0ab45724e86
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-08-17 11:40:42 +0330

    fix: widget repository store products

--- app/Modules/Core/Repositories/Widget/WidgetRepository.php
+++ app/Modules/Core/Repositories/Widget/WidgetRepository.php
@@ -4,8 +4,38 @@ namespace App\Modules\Core\Repositories\Widget;
 
 use App\Modules\Core\Models\Widget;
 use Celysium\Helper\Repository\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class WidgetRepository extends BaseRepository
 {
     protected static string $entity = Widget::class;
+
+    public function store(array $parameters): Model
+    {
+        DB::beginTransaction();
+
+        /** @var Widget $widget */
+        $widget = $this->model->newQuery()->create($parameters);
+
+        $widget->products()->attach($parameters['products_id']);
+
+        DB::commit();
+
+        return $widget->refresh();
+    }
+
+    public function update(Model $model, array $parameters): Model
+    {
+        DB::beginTransaction();
+
+        /** @var Widget $widget */
+        $model->update($parameters);
+
+        $widget->products()->sync($parameters['products_id']);
+
+        DB::commit();
+
+        return $widget->refresh();
+    }
 }
