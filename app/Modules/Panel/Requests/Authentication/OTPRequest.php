45940ec32c9310bcef0a358079971a2d178d5b9b
Nasser Hekmati <hekmati.nasser@gmail.com>
2024-09-11 16:55:37 +0330

    feat: check otp requests panel

---
+++ app/Modules/Panel/Requests/Authentication/OTPRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Modules\Panel\Requests\Authentication;
+
+use Celysium\Helper\Rules\Mobile;
+use Illuminate\Foundation\Http\FormRequest;
+
+class OTPRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'mobile' => ['required', new Mobile(), 'exists:users,mobile'],
+            'token'   => ['required', 'string']
+        ];
+    }
+}
